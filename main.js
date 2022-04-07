d6730753aa189c6a19e97ceaa6f919ef5af0b25f
carlo-ddm <carloduminuco90@gmail.com>
2022-04-07 18:28:54 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const menu = document.querySelector('.hamburger-menu')
 console.log(menu);
 
 hbMenu.addEventListener('click',function(){
-  // all'evento click deve comparire oppure scomparire tutto il menu in absolute
+  // all'evento click deve comparire il menu
   menu.classList.add("active");
+});
+
+const hbClose = document.querySelector('.hamburger-menu > a');
+console.log(hbClose);
+
+hbClose.addEventListener('click',function(){
+  // all'evento click deve scomparire il menu
+  menu.classList.remove("active");
 });
